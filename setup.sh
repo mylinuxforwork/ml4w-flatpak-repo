a6fabe112d00fa2b8aa399216cf8df78fd81d667
Stephan Raabe <mail@stephan-raabe.de>
2025-06-16 15:45:18 +0200

    Updates

---
+++ setup.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# ----------------------------------------------------------
+# Flatpak Information
+# ----------------------------------------------------------
+
+public_key="ml4w-apps-public-key.asc"
+public_key_url="https://mylinuxforwork.github.io/ml4w-flatpak-repo/$public_key"
+
+# ----------------------------------------------------------
+# Check if command exists
+# ----------------------------------------------------------
+
+_commandExists() {
+    local package="$1"
+    if ! type $package >/dev/null 2>&1; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+# ----------------------------------------------------------
+# Check if flatpak repo is installed
+# ----------------------------------------------------------
+
+_is_flatpak_repo_installed() {
+  local repo_name="$1"
+  flatpak_output=$(flatpak remotes)
+  if [[ $flatpak_output == *"$repo_name"* ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# ----------------------------------------------------------
+# Check if flatpak is already installed
+# ----------------------------------------------------------
+
+if ! _commandExists "flatpak"; then
+	echo "ERROR: Please install flatpak first."
+	exit
+fi
+
+# ----------------------------------------------------------
+# Check if wget is already installed
+# ----------------------------------------------------------
+
+if ! _commandExists "wget"; then
+	echo "ERROR: Please install wget first."
+	exit
+fi
+
+# ----------------------------------------------------------
+# Adding flathub
+# ----------------------------------------------------------
+
+if _is_flatpak_repo_installed "flathub"; then
+	echo ":: flathub is already added."
+else
+	echo ":: Adding flathub"
+	sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+fi
+
+# ----------------------------------------------------------
+# Adding ml4w-repo
+# ----------------------------------------------------------
+
+if _is_flatpak_repo_installed "ml4w-repo"; then
+	echo ":: ml4w-repo is already added."
+else
+	echo ":: Downloading Public Key"
+	if [ ! -d "$HOME/.cache" ]; then
+		mkdir -p "$HOME/.cache"
+	fi
+	if [ -f "$HOME/.cache/$public_key" ]; then
+		rm "$HOME/.cache/$public_key"
+	fi
+	wget -P "$HOME/.cache" "$public_key_url"
+	if [ ! -f "$HOME/.cache/$public_key" ]; then
+		echo "ERROR: Download of Public Key failed."
+		exit
+	fi	
+	echo ":: Adding ml4w-repo"
+	sudo flatpak remote-add --if-not-exists ml4w-repo https://mylinuxforwork.github.io/ml4w-flatpak-repo/ml4w-apps.flatpakrepo --gpg-import=$HOME/.cache/$public_key
+fi
+
+# ----------------------------------------------------------
+# Cleanup
+# ----------------------------------------------------------
+
+if [ -f "$HOME/.cache/$public_key" ]; then
+	rm "$HOME/.cache/$public_key"
+fi
+
+# ----------------------------------------------------------
+# Finishing up
+# ----------------------------------------------------------
+
+echo
+echo ":: ML4W Flatpak Repo added. You can check with flatpak remotes"
